9fb2ddbbd417f8b036ea700869347851fd0e7259
FlavioNoe <flavio.noe.santos@gmail.com>
2019-01-31 11:44:45 -0500

    Javascript commit

--- C0740832_Flavio_week4/js/main.js
+++ C0740832_Flavio_week4/js/main.js
@@ -1,1 +1,33 @@
-﻿// Seu código é inserido aqui!
+﻿var html;
+
+var date = new Date();
+
+html = "<h1> Date today is: " + date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + "</h1>";
+
+var day
+switch (date.getDay()) {
+    case 0: day = "Sunday"; break;
+    case 1: day = "Monday"; break;
+    case 2: day = "Tuesday"; break;
+    case 3: day = "Wednesday"; break;
+    case 4: day = "Thursday"; break;
+    case 5: day = "Friday"; break;
+    case 6: day = "Saturday"; break;
+}
+
+html = html + "<h2>and it is " + day + "</h2>"
+
+var num;
+num = Math.floor(Math.random() * 10); // number ramdon from 0 to 10...
+
+html = html + "<h2>A random number from 0 to 9: " + num + "</h2>"
+
+
+document.body.innerHTML = html;
+
+
+
+function myFunction() {
+    alert("my funtion works!!!")
+}
+
